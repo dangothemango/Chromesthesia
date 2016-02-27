0dac83402f80206d864a81510d3f8a82145cad3b
myoder345 <myoder345@gmail.com>
2016-02-27 18:50:45 -0500

    Began work on Platform Test
    I've got a test scene with a platform that phases in and out on the bass
    kicks. I understand how this system works now.

---
+++ Project Will Unity/Assets/PhasePlatform.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhasePlatform : MonoBehaviour {
+    public bool isSolid;
+    public GameObject AudioBeat;
+    MeshRenderer visual;
+    Collider collide;
+    float beatTimer = 0f;
+
+    void catchBeatSignals(BeatDetection.EventInfo eventInfo)
+    {
+        switch (eventInfo.messageInfo)
+        {
+            case BeatDetection.EventType.Kick:
+                if (beatTimer > .25)
+                {
+                    ToggleActive();
+                }
+                break;
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        visual = gameObject.GetComponent<MeshRenderer>();
+        collide = gameObject.GetComponent<Collider>();
+
+        visual.enabled = isSolid;
+        collide.enabled = isSolid;
+
+        AudioBeat.GetComponent<BeatDetection>().CallBackFunction = catchBeatSignals;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        beatTimer += Time.deltaTime;
+	}
+
+    void ToggleActive() {
+        isSolid = !isSolid;
+        visual.enabled = collide.enabled = isSolid;
+        print("Time between beats:" + beatTimer);
+        beatTimer = 0;
+    }
+}
